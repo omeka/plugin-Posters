3295a2ed4f8c4faf46160a5162be90abfe03c51e
Roberto Sanchez <rsanc77@yahoo.com>
2013-09-12 15:41:35 -0400

    skeleton upload

---
+++ PostersPlugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Center for History and New Media, 2010
+ * @package Posters
+ */
+
+ /**
+  * Posters plugin class
+  *
+  * @copyright Center for History and New Media, 2013
+  * @package Posters
+  */
+class PostersPlugin extends Omeka_Plugin_AbstractPlubin
+{
+    protected $_hooks = array();
+    protected $_filters = array();
+}
