b555c873de1c5f47075e5d566c7b23b75d6b65f2
Roberto Sanchez <rsanc77@yahoo.com>
2014-04-30 15:56:06 -0400

    pdf styling

--- models/Poster/Pdf.php
+++ models/Poster/Pdf.php
@@ -3,11 +3,12 @@
 class Poster_Pdf //extends Zend_Pdf{
 {
     protected $_pdf;
+    protected $_template;
     private $_font;
     private $_pageCount = 0;
 
-    const PAGE_HEIGHT = 792;
-    const PAGE_WIDTH  = 612;
+    const PAGE_HEIGHT = 595;
+    const PAGE_WIDTH  = 842;
 
     const MARGIN_LEFT   = 13.5;
     const MARGIN_RIGHT  = 36;
@@ -20,30 +21,64 @@ class Poster_Pdf //extends Zend_Pdf{
         // new pdf file will be created
         // everytime this class is called
         // this behaviour might change
+        $tmplt = dirname(__FILE__)."/PdfTemplate.pdf";
+        //var_dump($tmplt);
         $this->_pdf = new Zend_Pdf();
+        if (file_exists($tmplt)){
+            $this->_template = Zend_Pdf::load($tmplt);
+        }
     }
     public function generate($poster)
     {
 
-        $file = $poster->title . ".pdf";
+       $file = $poster->title . ".pdf";
+       $bfont = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
+       $font  = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
+       $index = 0;
+       $makePages = true;
+       while($makePages){
+           $page = $this->_pdf->newPage(Zend_Pdf_Page::SIZE_A4);
+          // 50 is the margin line
+           $x = 50;//$page->getWidth();
+           //start drawing from th
+           $y = 780;//$page->getHeight();
+           $page->setFont($bfont, 14);
+           $page->drawText($poster->title,$x,$y);
+           $y = $y - 12;// y = 778
+           $page->drawLine($x,$y, $x+495, $y-1);
+           $y = $y-12;//
+           if($index === 0){
+               
+               $this->_posterDesc($poster, $page, $x, $y);
+           }
+           
+      
+        $this->_pdf->pages[$index] = $page;
+       $makePages = false;
+       }
+        header("Content-Disposition: inline; filename=$file");
+        header("Content-type: application/x-pdf");
+        echo $this->_pdf->render();  exit;
         //create the pages object page size = 595:842
-        $page = $this->_pdf->newPage(Zend_Pdf_Page::SIZE_A4);
+       /* $page = $this->_pdf->newPage(Zend_Pdf_Page::SIZE_A4);
         $this->_pdf->pages[] = $page;
-        /**$page->setFont(Zend_Pdf_Font::fontWithName(
+        $page->setFont(Zend_Pdf_Font::fontWithName(
                 Zend_Pdf_Font::FONT_HELVETICA_BOLD
             ),
             14
         );
         $page->drawText($poster->title,50 ,760);
         $page->drawLine(50,755,545,755);
-         */
+        
         $this->_posterTitle($poster,$page);
         $this->_posterDesc($poster,$page);
         header("Content-Disposition: inline; filename=$file");
         header("Content-type: application/x-pdf");
         echo $this->_pdf->render();  exit;
-        //$this->_pdf->save($file);
+    //    //$this->_pdf->save($file);*/
+    
     }
+
     private function _getTempFilename($prefix, $count)
     {
           return $prefix . '-part' . $count . '.pdf';
@@ -55,23 +90,26 @@ class Poster_Pdf //extends Zend_Pdf{
         $page->drawLine(50,755,545,755);
 
      }
-    private function _posterDesc($poster, $page)
+    private function _posterDesc($poster, $page,$x ,$y)
     {
         $title = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
         $page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD), 12);
-        $page->drawText("Description: ",50, 740);
+        $page->drawText("Description: ",$x, $y);
+        $y = $y - 20;
         $page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 12);
-        $txtArr = $this->_cleanText($poster->description, 80);
+        $txtArr = $this->_cleanText($poster->description, 90);
         $i = 0;
-        $yaxis = 725;
+        $yaxis = $y;
         foreach($txtArr as $t){
-            $page->drawText($t,55,(725 - (13*$i)), 'UTF-8');
-            $yaxis = $yaxis - 13;   
+            $page->drawText($t,$x,$y, 'UTF-8');
+            $y = $y - 13;   
             $i++;
         }
+        $y = $y-7;
         $page->setFont($title,13);
-        $page->drawText("Poster Items: ",50, $yaxis);
-        $this->_posterItems($page,$poster,$yaxis-10);
+        $page->drawText("Poster Items: ",$x, $y);
+        $y = $y-20;
+        $this->_posterItems($page,$poster,$x, $y);
        
         //$page->drawText($this->_cleanText($poster->description),50,730, 'UTF-8');
         //exit;
@@ -79,30 +117,37 @@ class Poster_Pdf //extends Zend_Pdf{
         
     }
 
-     private function _posterItems($page, $poster, $yaxis)
+     private function _posterItems($page, $poster,$x, $y)
      {
-        $y1 = 525-5;
-        $y2 = 625-5;
+         $pages = array();
+         $pages[] = $page;
+       
         $title = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
         $page->setFont($title, 10);
         
                 
         foreach($poster->Items as $item){
-            $page->drawText(metadata($item, array('Dublin Core','Title')),50, $y2);
-            $y1 = $y1 - 13;
-            $y2 = $y2 - 13;
+            $y = $y- 13;
+            foreach($this->_cleanText(metadata($item, array('Dublin Core','Title')), 90) as $t){
+                $page->drawText($t,$x, $y);
+                $y = $y - 12;
+            }
+            $y2= $y;
+            
              if(metadata($item, 'has files')){
                  $i = 0;
                  foreach($item->Files as $file){
                     if($file->hasThumbnail()){
-                        $yaxis = $yaxis - ($i * 100);
                         $imgSrc = FILES_DIR."/".$file->getStoragePath();                      
                         $image = Zend_Pdf_Image::imageWithPath($imgSrc);
-                        //                image, x1, y1,    x2, y2
-                        $page->drawImage($image, 50,$y1,200, $y2);
-
-                        $y1 = $y1 - 110;
-                        $y2 = $y2 - 110;
+                        $y2 = $y - 110;//image, x1, y1,    x2, y2
+                        $page->drawImage($image, $x,$y2,($x + 150), $y);
+                        $page->drawText("",$x, $y);
+                        $y = $y- 10;
+                        $page->drawText("$y    $y2",$x,($y2 - 10));
+                        $y = $y - 110;
+                        
+                        //$page->drawText("$y    $y2",$x,487);
                     }
                 }
              }
