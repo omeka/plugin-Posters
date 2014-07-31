3bc1ec47281e0bdbaec1a5b9c32dab6b19628a75
Kim Nguyen <design@kimisgold.com>
2014-07-31 14:47:28 -0400

    Decruft print layout, style to have images float alongside their metadata like in static layout, tweak margins and borders.

--- views/public/posters/print.php
+++ views/public/posters/print.php
@@ -7,51 +7,48 @@
 <?php $pageTitle = 'Poster: &quot;' . html_escape($poster->title). '&quot;'; ?>
     <h1><?php echo $pageTitle; ?></h1>
     <hr>
-	<div id="poster">
+    <div id="poster">
         <div id="poster-info">
         <h2>Description:</h2>
-		<?php echo $poster->description; ?>
-		</div>
+        <?php echo $poster->description; ?>
+        </div>
 
-            <h2>Items:</h2>
+        <h2>Items:</h2>
         <ul class="poster-items">
         <?php foreach($poster->Items as $posterItem): ?>
-<?php    echo "<li>";
-echo "<h2>Title: ".metadata($posterItem, array('Dublin Core', 'Title'))."</h2>";
-echo  '<h3>Url: http://'.$_SERVER['HTTP_HOST'].html_escape(url("items/show/{$posterItem->id}"))."</h3>";
+        <li>
+            <?php
             if(metadata($posterItem, 'has files')){
                 foreach($posterItem->Files as $itemFile) {
                     if($itemFile->hasThumbnail()){
                         echo "<div class='item-file'>"
                             .link_to_item(file_image(get_option('poster_default_file_type_print'), array(),  $itemFile), array('class' =>'item-thumbnail'), 'show', $posterItem)
                             ."</div>";
-                            //."<div class='poster-item-annotation'>"
-                            //.$posterItem->caption
-                            //."</div>";
-                        //break;
                     }
                 }
             } 
-                echo "<div class='poster-item-annotation'>"
-                    .$posterItem->caption
-                    ."</div>";
-?>           
-               </li>
+            ?>
+            <h2>Title: "<?php echo metadata($posterItem, array('Dublin Core', 'Title')); ?>"</h2>
+            <h3>Url: <?php echo 'http://'.$_SERVER['HTTP_HOST'].html_escape(url("items/show/{$posterItem->id}")); ?>"</h3>
+            <div class='poster-item-annotation'>
+                <?php echo $posterItem->caption; ?>
+            </div>
+        </li>
         <?php endforeach; ?>
          </ul>
         <div class="pagebreak"></div>
-		<?php 
+        <?php 
          $disclaimer = get_option('poster_disclaimer');
-		 if (!empty($disclaimer)): 
-		?>
-		<div id="poster-disclaimer">
+         if (!empty($disclaimer)): 
+        ?>
+        <div id="poster-disclaimer">
             <h3 id="poster-disclaimer-title">Disclaimer:</h3>
             <p id="disclaimer-text">
             <?php echo html_escape($disclaimer); ?>
             </p>
-		</div>
+        </div>
         <?php endif; ?>
-       	</div>
+           </div>
 </div> <!-- end primary div -->
 </body>
 </html>
